d32186586a4f3f22aeaa0f686eff865ede1b3c35
Alex <alex.beciana@gmail.com>
2024-08-16 20:02:44 -0600

    Added Email factory

--- spec/factories/emails.rb
+++ spec/factories/emails.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :email do
-    
+    association :user
+    subject { Faker::Lorem.sentence }
+    body { Faker::Lorem.paragraph }
+    sent_at { Faker::Time.backward(days: 2, period: :evening) }
+
+    trait :with_events do
+      after(:create) do |email|
+        create_list(:email_event, 3, email: email)
+      end
+    end
   end
 end
