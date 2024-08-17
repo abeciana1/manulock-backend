80442c8dc4c071a5d762f043c235e970341fea1b
Alex <alex.beciana@gmail.com>
2024-08-16 20:02:02 -0600

    Created EmailEvents factory

--- spec/factories/email_events.rb
+++ spec/factories/email_events.rb
@@ -1,5 +1,51 @@
 FactoryBot.define do
   factory :email_event do
-    
+    association :email
+    email_client { %i[gmail outlook yahoo other].sample }
+    geolocation { "#{Faker::Address.latitude}, #{Faker::Address.longitude}" }
+    additional_data { { ip_address: Faker::Internet.ip_v4_address } }
+
+    # email clients
+    trait :gmail do
+      email_client { :gmail }
+    end
+
+    trait :outlook do
+      email_client { :outlook }
+    end
+
+    trait :yahoo do
+      email_client { :yahoo }
+    end
+
+    # event types
+    trait :sent do
+      event_type { :sent }
+    end
+
+    trait :opened do
+      event_type { :opened }
+    end
+
+    trait :clicked do
+      event_type { :clicked }
+    end
+
+    trait :bounced do
+      event_type { :bounced }
+    end
+
+    # device types
+    trait :desktop do
+      device_type { :desktop }
+    end
+
+    trait :mobile do
+      device_type { :mobile }
+    end
+
+    trait :tablet do
+      device_type { :tablet }
+    end
   end
 end
