f05e54302fa5c0e63440847366841820180f8703
Alex <alex.beciana@gmail.com>
2024-08-17 15:19:19 -0600

    Added enums and validations to annotation model

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -2,5 +2,11 @@ class Annotation < ApplicationRecord
     belongs_to :document
     has_many :comments, dependent: :destroy
 
-    validates :page_number, :selected_text, presence: true
+    enum annotation_type: {
+        highlight: 0,
+        comment: 1
+    }
+
+    validates :page_number, :selected_text, :annotation_type, presence: true
+    validates :annotation_type, numericality: { less_than_or_equal_to: 1 }
 end
