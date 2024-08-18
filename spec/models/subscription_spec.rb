e00f37b130e2835fced21af9ddc664d94afa798d
Alex <alex.beciana@gmail.com>
2024-08-17 23:30:34 -0600

    Added subscription model tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @starter_subscription = create(:subscription)
+    @premium_subscription = create(:subscription, :premium)
+    @pro_subscription = create(:subscription, :pro)
+  end
+  
+  describe "Validations" do
+    it { should validate_presence_of(:plan) }
+    it { should validate_numericality_of(:documents_remaining) }
+    it { should validate_numericality_of(:invites_remaining) }
+  end
+
+  describe "Associations" do
+    it { should belong_to(:user) }
+  end
+
+  it "starter subscription is valid" do
+    expect(@starter_subscription).to be_valid
+  end
+
+  it "premium subscription is valid" do
+    expect(@premium_subscription).to be_valid
+  end
+
+  it "pro subscription is valid" do
+    expect(@pro_subscription).to be_valid
+  end
+
 end
