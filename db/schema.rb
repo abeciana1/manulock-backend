856ba4697ff4286733d85632c8fe9a09fd7a560c
Alex <alex.beciana@gmail.com>
2024-08-17 22:34:17 -0600

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_17_211503) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_000047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -127,7 +127,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_211503) do
     t.string "last_name"
     t.string "email"
     t.integer "auth_id"
-    t.integer "role", default: 0
+    t.integer "role", default: 0, null: false
     t.bigint "invited_by_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
