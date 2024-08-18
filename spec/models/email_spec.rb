494745a7643ca19ca324641258c1acacc5c43824
Alex <alex.beciana@gmail.com>
2024-08-17 23:53:17 -0600

    Added Tests

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Email, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should belong_to :user }
+    it { should have_many(:email_events) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:subject) }
+    it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:sent_at) }
+  end
 end
