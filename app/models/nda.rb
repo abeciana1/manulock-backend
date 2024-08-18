6a2bbd805082a61a2de7dafb8dfa70442eaae093
Alex <alex.beciana@gmail.com>
2024-08-17 23:46:02 -0600

    Added tests

--- app/models/nda.rb
+++ app/models/nda.rb
@@ -1,12 +1,12 @@
 class Nda < ApplicationRecord
-    belongs_to :user
-    belongs_to :document
-
-    validates :signed_at, presence: true, if: -> { signed? }
-    validates :pdf_url, presence: true, if: -> { signed? }
-
     enum signed: {
         unsigned: 0,
         signed: 1
     }
+    
+    belongs_to :user
+    belongs_to :document
+
+    validates :signed_at, :pdf_url, presence: true, unless: :signed?
+
 end
