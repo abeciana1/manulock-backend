e92a7b6d7b59d36065bcd2f37665df377189e4d9
Alex <alex.beciana@gmail.com>
2024-10-05 13:39:17 -0600

    Mapped user properties for creation if user auth token is not found

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -13,12 +13,15 @@ module Authentication
         clerk_user_id = request.headers['Clerk-User-Id']
         if clerk_user_id.present?
             @current_user = User.find_or_create_by(auth_id: clerk_user_id) do |user|
-                user.first_name = user_login_params[:first_name]
-                user.last_name = user_login_params[:last_name]
-                user.email = user_login_params[:email]
+                # user.first_name = user_login_params[:first_name]
+                # user.last_name = user_login_params[:last_name]
+                # user.email = user_login_params[:email]
+                user.first_name = params[:user][:first_name] if params[:user].present?
+                user.last_name = params[:user][:last_name] if params[:user].present?
+                user.email = params[:user][:email] if params[:user].present?
                 user.role = "inviter"
             end
-            render json: { user: UserSerializer.new(@current_user) }, status: :success
+            return @current_user
         else
             render json: { error: 'Auth header not found — unauthorized' }, status: :unauthorized
         end
