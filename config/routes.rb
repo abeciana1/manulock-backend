29086c0957f04f5b801d6750c48aefa6c8d7926c
Alex <alex.beciana@gmail.com>
2024-11-20 16:20:50 -0700

    Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :documents
-      get 'my-documents', to: 'user#my_documents'
+      get 'my-documents', to: 'users#my_documents'
     end
   end
 end
